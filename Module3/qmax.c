6e5039542c5b5eab545fb5b4ed526f1bf1f5e8e4
Александр Никитин <121054417+Olegsandrik@users.noreply.github.com>
2022-12-30 15:17:46 +0300

    Update qmax.c

--- Module3/qmax.c
+++ Module3/qmax.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <iso646.h>
-typedef struct{ long long value, max;}   node;
+typedef struct{long long value, max;}   node;
 //Реализация двустороннего стека, где top1 - обычное начало стека, а top2 - с конца стека идет
 typedef struct{node* data;long long top1, top2, cap;} doubleStack;
 doubleStack initDoubleStack(long long length){
     doubleStack stack;
-    stack.data = (node*)calloc(length, sizeof(node));
+    stack.data = (node*)calloc(length,sizeof(node));
     stack.cap = length;
     stack.top1 = 0;
     stack.top2 = length - 1;
@@ -22,11 +22,14 @@ char stackEmpty2(doubleStack stack){
     return stack.top2 == stack.cap - 1;
 }
 char doubleStackEmpty(doubleStack stack){
-    return stackEmpty1(stack) and stackEmpty2(stack);
+    return stackEmpty1(stack) && stackEmpty2(stack);
 }
 void push1(doubleStack *stack, long long value){
     node node = stack->data[stack->top1];
-    if((stackEmpty1(*stack)!=1)){
+    if((stackEmpty1(*stack))){
+        node.max = value;
+    }
+    else {
         long long prevmax = stack->data[stack->top1 - 1].max;
         if (value > prevmax){
             node.max = value;
@@ -35,26 +38,23 @@ void push1(doubleStack *stack, long long value){
             node.max = prevmax;
         }
     }
-    else {
-        node.max = value;
-    }
     node.value = value;
     stack->data[stack->top1] = node;
     stack->top1++;
 }
 void push2(doubleStack *stack, long long value){
     node node = stack->data[stack->top2];
-    if((stackEmpty2(*stack))!=0){
-        long long prevmax = stack->data[stack->top2 + 1].max;
-        if(value > prevmax){
+    if(stackEmpty2(*stack)){
+        node.max = value;
+    }
+    else {
+        long long prevmax = stack->data[stack->top1 + 1].max;
+        if (value > prevmax){
             node.max = value;
         }
         else{
             node.max = prevmax;
         }
-    }
-    else {
-        node.max = value;
     }
     node.value = value;
     stack->data[stack->top2] = node;
@@ -73,11 +73,9 @@ void enqueue(doubleStack *stack, long long value){
     push1(stack, value);
 }
 node dequeue(doubleStack *stack){
-    if(stackEmpty2(*stack)){
-        while(!stackEmpty1(*stack)){
+    if(stackEmpty2(*stack))
+        while(!stackEmpty1(*stack))
             push2(stack, pop1(stack).value);
-        }
-    }
     return pop2(stack);
 }
 long long max(long long a, long long b){
@@ -87,24 +85,20 @@ long long max(long long a, long long b){
         return a;
     }
 }
-int main(){
+int main() {
     long long n;
     scanf("%lld", &n);
     doubleStack stack = initDoubleStack(100001);
     int i=0;
-    while(i < n){
-        char command[7];
+    while ( i < n) {
+        char command[10];
         scanf("%s", command);
-        if(!strcmp("DEQ", command)) {
-            printf("%lld\n", dequeue(&stack).value);
-            continue;
-        }
-        else if(!strcmp("MAX", command)) {
-            if(stackEmpty2(stack)) {
+        if (!strcmp("MAX", command)) {
+            if (stackEmpty2(stack)) {
                 printf("%lld\n", stack.data[stack.top1 - 1].max);
                 continue;
             }
-            if(!stackEmpty1(stack) and !stackEmpty2(stack)) {
+            if (!stackEmpty1(stack) and !stackEmpty2(stack)) {
                 printf("%lld\n", max(stack.data[stack.top1 - 1].max, stack.data[stack.top2 + 1].max));
                 continue;
             }
@@ -113,14 +107,18 @@ int main(){
                 continue;
             }
         }
-        else if(!strcmp("ENQ", command)) {
+        else if (!strcmp("DEQ", command)) {
+            printf("%lld\n", dequeue(&stack).value);
+            continue;
+        }
+        else if (!strcmp("ENQ", command)) {
             long long x;
             scanf("%lld", &x);
             enqueue(&stack, x);
             continue;
         }
-        else if(!strcmp("EMPTY", command)) {
-            if(doubleStackEmpty(stack)) printf("true\n");
+        else if (!strcmp("EMPTY", command)) {
+            if (doubleStackEmpty(stack)) printf("true\n");
             else printf("false\n");
             continue;
         }
